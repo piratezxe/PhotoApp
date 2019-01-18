1f57fb9df2915d2eb9e35b7f01538e0ccdcd46a0
zxcdsa <a1892412@nwytg.net>
2019-01-18 11:27:08 +0100

    some detail

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,13 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Start
+                    Twoj Profil
+                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Szukaj
+                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Ranking
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
